6433705ec5bd072f793543c1276caf29473ec411
Porokhnya_AN <Porokhnya.A.N@yandex.kz>
2023-03-20 13:04:12 +0600

    - добавления сервиса для чтения файла, класс ReadFile изменен. - добавлен метод readTheFile(имя файла), возращаемое значение "лист строк"

--- src/main/java/org/example/services/read_file/ReadFile.java
+++ src/main/java/org/example/services/read_file/ReadFile.java
@@ -1,4 +1,19 @@
 package org.example.services.read_file;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class ReadFile {
+    public List<String> readTheFile(String path){
+        try(Stream<String> stream= new BufferedReader(new FileReader(path)).lines();){
+            return stream.collect(Collectors.toList());
+        }
+        catch(Exception e){
+            e.getStackTrace();
+        }
+        return null;
+    }
 }
