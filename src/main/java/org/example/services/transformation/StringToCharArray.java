2c07969fde5e14879a1fea553ce9bd7a30fa93b4
Porokhnya <Porokhnya.A.N@yandex.kz>
2023-03-26 17:57:21 +0600

    реализация класса:  создание переменных для маштабирования двухмерного массива  создания метода для преобразования List<String> в двухмерный массив

--- src/main/java/org/example/services/transformation/StringToCharArray.java
+++ src/main/java/org/example/services/transformation/StringToCharArray.java
@@ -1,4 +1,24 @@
 package org.example.services.transformation;
 
+import java.util.List;
+
 public class StringToCharArray {
+    private int widthArray;
+    private int heightArray;
+
+    public StringToCharArray(int widthArray, int heightArray) {
+        this.widthArray = widthArray;
+        this.heightArray = heightArray;
+    }
+
+    public char[][] transformationToArray(List<String> list) {
+        int countHeight = 0;
+        char[][] array = new char[heightArray][widthArray];
+        for (String s : list) {
+            array[countHeight] = s.toCharArray();
+            countHeight++;
+        }
+        return array;
+    }
 }
+
